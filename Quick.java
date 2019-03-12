85ad3f5ec11c5c68116c6ba87acdc200c8f9c2ae
Amanda Chen <achen01@stuy.edu>
2019-03-11 20:47:06 -0400

    fixed parition code when randomly chosen int is the 0th idx

--- Quick.java
+++ Quick.java
@@ -4,18 +4,18 @@ public class Quick{
 
   public static int partition(int[] data, int start, int end){
     int random = (int)(Math.random() * (end-start));
-    System.out.println("random : "+random);
+    //System.out.println("random : "+random);
     int ori = start;
     int temp = data[random + start];
-    System.out.println("temp: " + temp);
+    //System.out.println("temp: " + temp);
     if (data.length <= 1){
       return 0;
     }
     data[random + start] = data[start];
-    System.out.println(Arrays.toString(data));
+    //System.out.println(Arrays.toString(data));
     data[start] = temp;
     while (start != end){
-      System.out.println(Arrays.toString(data));
+      //System.out.println(Arrays.toString(data));
       if (data[start] > data[ori]){
         temp = data[start];
         data[start] = data[end];
@@ -32,9 +32,12 @@ public class Quick{
       return start;
     } else {
       temp = data[ori];
-      data[ori] = data[start-1];
-      data[start-1] = temp;
-      return start - 1;
+      if (ori != 0){
+        data[ori] = data[start-1];
+        data[start-1] = temp;
+        return start - 1;
+      }
+      return start;
     }
   }
 
@@ -44,8 +47,9 @@ public class Quick{
    int s = 0;
    int e = data.length - 1;
    int i = partition(data, s, e);
+   System.out.println("i: "+i);
    while(i != k){
-     System.out.println(Arrays.toString(data));
+     System.out.println("loop start: " +Arrays.toString(data));
      if (i < k){
        System.out.println("i<k");
        s = i+1;
@@ -61,14 +65,14 @@ public class Quick{
 
  public static void main(String[] args){
    int[]ary = { 2, 10, 15, 23, 0,  5};  //sorted :  {0,2,5,10,15,23}
-   /*System.out.println(quickselect( ary , 0 ));// would return 0
-   System.out.println(quickselect( ary , 1 ));//  would return 2
-   System.out.println(quickselect( ary , 2 ));//  would return 5
-   System.out.println(quickselect( ary , 3 ));//   would return 10
-   System.out.println(quickselect( ary , 4 ));//  would return 15
-   System.out.println(quickselect( ary , 5 ));//  would return 23*/
-   System.out.println(partition(ary,1,5));
-   System.out.println(Arrays.toString(ary));
+   System.out.println(quickselect( ary , 0 ));// would return 0
+   //System.out.println(quickselect( ary , 1 ));//  would return 2
+   //System.out.println(quickselect( ary , 2 ));//  would return 5
+   //System.out.println(quickselect( ary , 3 ));//   would return 10
+   //System.out.println(quickselect( ary , 4 ));//  would return 15
+   //System.out.println(quickselect( ary , 5 ));//  would return 23
+   //System.out.println(partition(ary,0,3));
+   //System.out.println(Arrays.toString(ary));
  }
 
 
