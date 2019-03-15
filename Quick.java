afc1e2408b3ab331deb4583ed09bac5a339af307
Amanda Chen <achen01@stuy.edu>
2019-03-14 21:56:04 -0400

    added comments and tried to make sort faster

--- Quick.java
+++ Quick.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 public class Quick{
 
   public static int partition(int[] data, int start, int end){
-    if (end-start < 1){
+    if (end-start < 1){ //returns start is size is less than 2
       return start;
     }
     int i = start;
     int j = end;
-    int middle = (int)(Math.random() * (j-i));
+    int middle = (i+j)/2;
     //System.out.println("random : "+random);
-    int pivot = middle;
+    int pivot = middle; //changes pivot to median on start, end, and middle values
     if (data[i] >= data[j] && data[j] >= data[middle]){
       pivot = j;
     } else if (data[i] <= data[j] && data[j] <= data[middle]){
@@ -20,13 +20,13 @@ public class Quick{
     } else if (data[j] <= data[i] && data[i] <= data[middle]){
       pivot = i;
     }
-    int temp = data[pivot];
+    int temp = data[pivot]; //swaps pivot to beginning
     //System.out.println("temp: " + temp);
     data[pivot] = data[start];
     //System.out.println(Arrays.toString(data));
     data[start] = temp;
     i++;
-    while (i != j){
+    while (i != j){ //loops through array and swaps when i is > or = to the pivot
       //System.out.println(Arrays.toString(data));
       if (data[i] > data[start]){
         temp = data[i];
@@ -34,7 +34,7 @@ public class Quick{
         data[j] = temp;
         j--;
       } else if (data[i] == data[start]){
-        if ((int)(Math.random() + .5) == 0){
+        if ((int)(Math.random() + .5 ) == 0){
           temp = data[i];
           data[i] = data[j];
           data[j] = temp;
@@ -47,13 +47,13 @@ public class Quick{
       }
     }
     //System.out.println("i:"+i);
-    if (data[start] > data[i]){
+    if (data[start] > data[i]){ //swaps pivot to where i if pivot is > the int a i
       temp = data[start];
       data[start] = data[i];
       data[i] = temp;
       //System.out.println("iia:"+i);
       return i;
-    } else {
+    } else { //swaps pivot to the index before i
       temp = data[start];
       data[start] = data[i-1];
       data[i-1] = temp;
@@ -67,39 +67,39 @@ public class Quick{
  public static int quickselect(int[]data, int k){
    int s = 0;
    int e = data.length - 1;
-   int i = partition(data, s, e);
+   int i = partition(data, s, e); //first partition to find a center
    //System.out.println("iii: "+i);
-   while(i != k && s != e){
+   while(i != k && s != e){ //the kth number has not been found and starting ind is != ending idx
      //System.out.println("loop start: " +Arrays.toString(data) + " i: "+i);
      if (i < k){
        //System.out.println("1.i<k, s: " + s + ", e: "+e+", k: "+k+", i: "+i);
        s = i+1;
-       i = partition(data,s,e);
+       i = partition(data,s,e); //parition right side
        //System.out.println("2.i<k, s: " + s + ", e: "+e+", k: "+k+", i: "+i);
      } else {
        //System.out.println("i>=k, s: " + s + ", e: "+e+", k: "+k+", i: "+i);
        e = i-1;
-       i = partition(data,s,e);
+       i = partition(data,s,e); //partition left side
        //System.out.println("i>=k, s: " + s + ", e: "+e+", k: "+k+", i: "+i);
      }
    }
    //System.out.println("loop end: " +Arrays.toString(data));
-   return data[k];
+   return data[k]; //return the value
  }
 
  /*Modify the array to be in increasing order.
  */
- public static void quicksort(int[] data){
-   sortH(data,0,data.length-1);
+ public static void quicksort(int[] data){ //wrapper
+   sortH(data,0,data.length-1); //helper
  }
 
  public static void sortH(int[] data, int lo, int hi){
-   if (lo >= hi){
+   if (lo >= hi){ //only 1 int
      return;
    }
-   int pivot = partition(data,lo,hi);
-   sortH(data,pivot+1,hi);
-   sortH(data,lo,pivot-1);
+   int pivot = partition(data,lo,hi); //places one value in proper position
+   sortH(data,pivot+1,hi); //sorts right side
+   sortH(data,lo,pivot-1); //sorts left side
  }
 
  public static void sortH2(int[] data, int lo, int hi){
@@ -134,7 +134,6 @@ public class Quick{
         data[lt] = temp;
         i++;
       } else {
-        lt++;
         i++;
       }
     }
@@ -156,20 +155,40 @@ public class Quick{
     //return an array [lt,gt]
 }
 
- public static void main(String[] args){
-   int[]ary = {2, 10, 15, 23, 0, 5};  //sorted :  {0,2,5,10,15,23}
-   //for (int i = 0; i < 10; i++ ){System.out.println((int)(Math.random() + .5));}
-   //System.out.println(quickselect( ary , 0 ));// would return 0
-   //System.out.println(quickselect( ary , 1 ));//  would return 2
-   //System.out.println(quickselect( ary , 2 ));//  would return 5
-   //System.out.println(quickselect( ary , 3 ));//   would return 10
-   //System.out.println(Quick.quickselect( ary , 4 ));//  would return 15
-   //System.out.println(quickselect( ary , 5 ));//  would return 23
-   //for (int i = 1; i < 6; i++){
-     //System.out.println(partition(ary,0,ary.length-1));
-     //System.out.println(Arrays.toString(ary));
-   //}
+public static void main(String[]args){
+ System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+ int[]MAX_LIST = {1000000000,500,10};
+ for(int MAX : MAX_LIST){
+   for(int size = 31250; size < 2000001; size*=2){
+     long qtime=0;
+     long btime=0;
+     //average of 5 sorts.
+     for(int trial = 0 ; trial <=5; trial++){
+       int []data1 = new int[size];
+       int []data2 = new int[size];
+       for(int i = 0; i < data1.length; i++){
+         data1[i] = (int)(Math.random()*MAX);
+         data2[i] = data1[i];
+       }
+       long t1,t2;
+       t1 = System.currentTimeMillis();
+       Quick.quicksort(data2);
+       t2 = System.currentTimeMillis();
+       qtime += t2 - t1;
+       t1 = System.currentTimeMillis();
+       Arrays.sort(data1);
+       t2 = System.currentTimeMillis();
+       btime+= t2 - t1;
+       if(!Arrays.equals(data1,data2)){
+         System.out.println("FAIL TO SORT!");
+         System.exit(0);
+       }
+     }
+     System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+   }
+   System.out.println();
  }
+}
 
 
 }
